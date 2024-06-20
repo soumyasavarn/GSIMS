0c288c29492cb94cdd0a027c263356e2ae3d1692
Soumya Savarn <soumyasavarn2@gmail.com>
2024-06-20 14:04:07 +0530

    Update grocery.sql

--- Database/grocery.sql
+++ Database/grocery.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 01, 2018 at 07:08 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,12 +6,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `grocery`
 --
 
